1fd975972a10b7c10920342962700d939db8cfad
bearsprogrammer <qpteman@hanyang.ac.kr>
2026-09-19 14:01:11 +0900

    edit .gitignore

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 from dataloader.nusc_loader import NuScenesloader
 from tracking.nusc_tracker import Tracker
 from nuscenes.nuscenes import NuScenes
+from auto_finetune_module.utils.config_dict_tree import ConfigDictTree
 from data.script.NUSC_CONSTANT import *
 from utils.io import load_file, dict_to_yaml
 from utils.script import tra_trans_det
@@ -289,8 +290,6 @@ def linear_search_parameters(online_cfg, intervals: List[List], steps: List[floa
     :return: parameter under the best performance
     """
     # load raw config
-    from auto_finetune_module.utils.config_dict_tree import ConfigDictTree
-
     online_config = copy.deepcopy(online_cfg)
 
     # check the sort metric
